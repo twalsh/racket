9c8d225330b7e4c9b4c9b345eead625a65ca1e19
tom <tom@celestica.(none)>
2011-02-05 21:32:00 +0000

    Add CD DB.

---
+++ cd.scm
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (make-cd title artist rating ripped)
+  (make-hash (list (cons 'title title) (cons 'artist artist) (cons 'rating rating)
+                   (cons 'ripped ripped))))
+
+(define *db* '())
+
+(define (add-record cd)
+  (set! *db* (cons cd *db*)))
+
+(define (dump-db)
+  (for-each (lambda (cd) (printf "~a / ~a / ~a ~n" 
+                                 (hash-ref cd 'title)
+                                 (hash-ref cd 'artist)
+                                 (hash-ref cd 'rating)
+                                 )
+                                 
+              ) 
+            *db*))
+
+(define cd1 (make-cd "Undertow" "Warpaint" 10 #t))
+(define cd2 (make-cd "Metal Heart" "Cat Power" 9 #t))
+(define cd3 (make-cd "Sugar" "Ladytron" 8 #t))
+
+(for-each add-record (list cd1 cd2 cd3))
+
