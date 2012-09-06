07362064d8dd5c4373fd74b3365404caa7e1e279
tom <tom@tom-EQUIUM-A100.(none)>
2012-09-06 20:24:01 +0100

    Added header and proper formatting.

--- vcf.rkt
+++ vcf.rkt
@@ -5,9 +5,12 @@
 
 (define nuc (set #\A #\C #\G #\T))
 
+(printf "~a\n" (string-join '("#CHROM" "POS" "ID" "REF" "ALT" "QUAL" "FILTER" "INFO") "\t"))
+
 (for/fold ((i 0))
   ((c (in-string fasta)) #:unless (char=? c #\N))
-  (let ((diff (list->string (set->list (set-remove nuc c)))))
-    (printf "~a ~a ~a\n" c (+ i 1) diff))
+  (let ((alt (list->string (set->list (set-remove nuc c)))))
+    (printf "~a\n" ;"~a\t~a\t~a\t~a\t~a\t~a\t~a\t~a\n" 
+            (string-join (list "chr" (number->string (+ i 1)) "." (string c) "alt" "100" "PASS" "DP=100") "\t")))
   (+ i 1)
   )
