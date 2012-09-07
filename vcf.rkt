df03fab123a499116e75a7a41cde66bcb6cb484f
Tom Walsh <welisc@yahoo.com>
2012-09-07 09:29:56 +0100

    Replace set with hash.

--- vcf.rkt
+++ vcf.rkt
@@ -3,13 +3,14 @@
 
 (define fasta "ACCTGAGACTGACNTGACTTGACTGA")
 
-(define nuc (set #\A #\C #\G #\T))
+(define nuc (make-hash))
+(hash-set*! nuc #\A "CGT" #\C "AGT" #\G "ACT" #\T "ACG")
 
 (printf "~a\n" (string-join '("#CHROM" "POS" "ID" "REF" "ALT" "QUAL" "FILTER" "INFO") "\t"))
 
 (for/fold ((i 0))
   ((c (in-string fasta)) #:unless (char=? c #\N))
-  (let ((alt (list->string (set->list (set-remove nuc c)))))
+  (let ((alt (hash-ref nuc c)))
     (printf "~a\t~a\t~a\t~a\t~a\t~a\t~a\t~a\n" 
             "chr" (+ i 1) "." c alt "100" "PASS" "DP=100"))
   (+ i 1)
