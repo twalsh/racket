f2eeaa0c4395a321adfcf211ec199c9206d481da
Tom Walsh <walshtp@gmail.com>
2013-10-16 15:03:43 +0100

    Parse book entries

--- lt.rkt
+++ lt.rkt
@@ -2,17 +2,29 @@
 (require json)
 (require net/url)
 
-(define userid "welisc")
-(define key 3366634915)
-
 (define base-addr "http://www.librarything.com/api_getdata.php")
 (define base-url (string->url base-addr))
 
-(define query (format "?userid=~a&key=~a" userid key))
+(define userid "welisc")
+(define key 3366634915)
+(define max 10)
+(define query (format "?userid=~a&key=~a&responseType=json&max=~a" userid key max))
+
 (define query-url (combine-url/relative base-url query))
+(define js (read-json (get-pure-port query-url)))
 
-(define fetch (get-pure-port query-url))
-(define s (substring (string-trim (read-line fetch) #px";LibraryThing.+") 20))
-(define js (string->jsexpr s))
+;(display js)
 
-(display js)
+(define books (hash-ref js 'books))
+(define book-count (length (hash-keys books)))
+;(display books)
+(printf "~a books~n" book-count)
+
+(define (print-book id book-data)
+  (printf "~a " id)
+  (printf "~a " (hash-ref book-data 'title))
+  (printf " / ~a " (hash-ref book-data 'author_fl))
+  (printf "~n"))
+
+
+(hash-for-each books print-book)
