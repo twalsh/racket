09df3c69bedee799a196414eb80c2cde0cbda417
Tom Walsh <walshtp@gmail.com>
2013-10-16 13:28:38 +0100

    Added LT

---
+++ lt.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require json)
+(require net/url)
+
+(define userid "welisc")
+(define key 3366634915)
+
+(define base-addr "http://www.librarything.com/api_getdata.php")
+
+(define base-url (string->url base-addr))
+
+(define query (format "?userid=~a&key=~a" userid key))
+
+(define query-url (combine-url/relative base-url query))
+
+(define fetch (get-pure-port query-url))
+
+(define s (substring (string-trim (read-line fetch) #px";LibraryThing.+") 20))
+
+(define js (string->jsexpr s))
+
+(display js)
