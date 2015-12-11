04119ff26338de6d20d845b6a4a00c45a7320495
Tom Walsh <walshtp@gmail.com>
2015-12-11 10:46:33 +0000

    Add advent

---
+++ advent/advent1.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define input
+  (string->list
+   (port->string (open-input-file "input1.txt"))))
+
+(let loop
+           (
+            (orders input)
+            (floor 0)
+            (pos 0)
+            (first-entry 0)
+           )
+  (if (empty? orders)
+      (cons floor first-entry)
+      
+      (let ((order (car orders)))
+          (loop 
+                 (cdr orders)
+                 (if (eq? order #\()
+                     (+ floor 1)
+                     (- floor 1))
+                 (+ pos 1)
+                 (if (and (= floor -1) (= first-entry 0))
+                     pos
+                     first-entry)))))
+                  
+      
