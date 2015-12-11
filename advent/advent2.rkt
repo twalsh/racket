e8a961207a5b660b9d56f325290fd4c8687fb054
Tom Walsh <walshtp@gmail.com>
2015-12-11 13:02:33 +0000

    Day 2 complete

--- advent/advent2.rkt
+++ advent/advent2.rkt
@@ -1,23 +1,28 @@
 #lang racket
 
-(define (wrap-present line)
-  (let ((dimensions (map string->number (string-split line "x"))))
-    (match-let (((list l w h) dimensions))
+(define (get-ribbon dimensions)
+  (+ (* 2 (apply + (take (sort dimensions <) 2)))
+     (apply * dimensions)))
+
+(define (get-paper dimensions)
+  (match-let (((list l w h) dimensions))
     (+ (* 2
           (+
            (* l w)
            (* w h)
            (* h l)))
        ; Extra paper
-       (apply * (take (sort dimensions <) 2))))))
+       (apply * (take (sort dimensions <) 2)))))
 
 (define (read-input in)
-  (let loop ((line (read-line in)) (total-area 0))
+  (let loop ((line (read-line in)) (total-paper 0) (total-ribbon 0))
     (if (eof-object? line)
-        total-area
-        (let ((area (wrap-present line)))
-          (loop (read-line in) (+ total-area (wrap-present line))
-                )))))
+        (cons total-paper total-ribbon)
+        (let ((dimensions (map string->number (string-split line "x"))))
+          (let ((paper (get-paper dimensions))
+                (ribbon (get-ribbon dimensions)))
+            (loop (read-line in) (+ total-paper paper) (+ total-ribbon ribbon))
+            )))))
 
 
 (call-with-input-file "input2.txt" read-input)
