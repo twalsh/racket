e4c6f6d766278062daa6f9ce03f8fd76095f02a8
Tom Walsh <walshtp@gmail.com>
2015-12-14 15:55:35 +0000

    advent4.rkt

--- advent/advent4.rkt
+++ advent/advent4.rkt
@@ -2,10 +2,10 @@
 
 (require openssl/md5)
 
-(define key "abcdef")
+(define key "bgvyzdsv")
 
-(let loop ((i 609000))
-  (display i)(newline)
+(let loop ((i 0))
+  ;(display i)(newline)
   (let ((string (string-join  (list key (number->string i)) "")))
     (let ((hash (md5 (open-input-string string))))
       (if (string=? (substring hash 0 5) "00000")
