641b310b9e5309797d2d339a293ff3238e3faeef
Tom Walsh <walshtp@gmail.com>
2015-12-14 15:44:35 +0000

    Remove redundant places-visited function and reorganize visit-map

--- advent/advent3.rkt
+++ advent/advent3.rkt
@@ -5,11 +5,8 @@
    (port->string (open-input-file "input3.txt"))))
 
 (define (visit-map order-list init-visits)
-  ;(display order-list)(newline)
   (let loop
-    (
-     (x 0)
-     (y 0)
+    ((x 0) (y 0)
      (visits init-visits)
      (orders order-list))
     (if (empty? orders)
@@ -24,31 +21,23 @@
             
             (loop x y (hash-update visits (cons x y) add1 0) (cdr orders)))))))
 
-(define (places-visited visit-map)
-  (length (filter (lambda (h)
-                    (>= (cdr h) 1))
-                  (hash->list visit-map
-                   
-                   ))))
 ; 3a answer
-(places-visited (visit-map input (make-immutable-hash)))
-
+(hash-count (visit-map input (make-immutable-hash)))
 ; 3b
 (define split-orders
-  (let loop ((santa '())
-             (robo-santa '())
+  (let loop ((split (list '() '()))
              (orders input))
     (if (empty? orders)
-        (list (reverse santa) (reverse robo-santa))
-        (loop (cons (car orders) santa)
-              (cons (cadr orders) robo-santa)
+        (map reverse split)
+        (loop (list
+               (cons (car orders) (car split))
+               (cons (cadr orders) (cadr split)))
               (cddr orders)))))
 
 (define santa-visits (visit-map (car split-orders) (make-immutable-hash)))
 (define all-visits (visit-map (cadr split-orders) santa-visits))
-
 ; 3b answer
-(places-visited all-visits)
+(hash-count all-visits)
 
 
       
