1cf37d2db26140595dd9d0faf72bd6d78b1f6ee0
Tom Walsh <walshtp@gmail.com>
2015-12-15 08:56:05 +0000

    Add rackunit

--- advent/advent5.rkt
+++ advent/advent5.rkt
@@ -1,14 +1,19 @@
 #lang racket
+(require rackunit)
+
 (define nice-s (list "ugknbfddgicrmopn" "aaa"))
 
 (define naughty-s (list "jchzalrnumimnmhp" "haegwjzuvuyypxyu" "dvszwmarrgswjxmb"))
 
+; At least 3 vowels
 (define (rule1 s)
   (>= 3 (length (regexp-match* #rx"[aeiou]" s))))
 
+; At least 1 doubled letter
 (define (rule2 s)
   (not (empty? (regexp-match* #px"([a-z])\\1" s))))
-
+s
+; No ab,cd,pq or xy
 (define (rule3 s)
   (not (regexp-match #rx"(ab|cd|pq|xy)" s)))
 
@@ -21,4 +26,4 @@
 (map rule3 nice-s)
 (map rule3 naughty-s)
 
-
+(check-equal? (rule1 (car nice-s)) #t "rule 1") 
