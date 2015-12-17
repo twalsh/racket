b4b416a3f484057912d80f540d60f6bff80e3e87
Tom Walsh <walshtp@gmail.com>
2015-12-17 08:28:28 +0000

    Add end-year function

--- hamur.rkt
+++ hamur.rkt
@@ -13,8 +13,6 @@
 
 (define _yield 3)
 
-(define (mortality-rate year starved pop) (exact-round (/ (+ (* (- year 1) mortality-rate) (/ (* starved 100) pop)) year)))
-
 (define (plague pop) (if (< (random) 0.15)
                          (/ pop 2)
                          0))
@@ -22,8 +20,6 @@
 (define (acres harvest _yield) (/ harvest _yield))
 
 (define (new-born pop acres grain) (exact-round (+ (/ (/ (* (random 6) (+ (* 20 acres) grain)) pop) 100) 1)))
-
-(define (end-game 'Done))
 
 (define (report-feed feed)
   (printf "feed ~a, sufficient for ~a people~n" feed (/ feed 20)))
@@ -36,16 +32,16 @@
             (letrec ((planted (plant-crops pop acres grain))
                      (harvested (+ (* planted _yield)))
                      (round (* harvested (random) 0.2)))
-                (letrec ((diseased (plague pop))
-                         (born (new-born pop acres grain))
-                         (starved (max (- pop (/ feed 20)) 0)))
-                  (new-year (+ year 1)
-                            (- (+ pop born) (+ starved diseased))
-                            (+ acres d-acres)
-                            (- grain (* d-acres price))
-                            planted harvested ratfood
-                            born diseased starved))))))
-      (end-game)))
+              (letrec ((diseased (plague pop))
+                       (born (new-born pop acres grain))
+                       (starved (max (- pop (/ feed 20)) 0)))
+                (new-year (+ year 1)
+                          (- (+ pop born) (+ starved diseased))
+                          (+ acres d-acres)
+                          (- grain (* d-acres price))
+                          planted harvested ratfood
+                          born diseased starved))))))
+      (end-game pop acres total-deaths)))
 
 (define (distribute-grain pop acres grain)
   (prompt (format "How many bushels do you wish to feed your people (0-~a)?" grain)
@@ -55,8 +51,8 @@
 (define (trade-acres pop acres grain price)
   (let ((buy
          (prompt (format "How many acres do you wish to buy (0-~a)" (floor (/ grain price)))
-                     (lambda (n) (<= (* n price) grain))
-                     (format "Hammurabi: Think again. You have only ~a bushels of grain." grain))))
+                 (lambda (n) (<= (* n price) grain))
+                 (format "Hammurabi: Think again. You have only ~a bushels of grain." grain))))
     (if (> buy 0)
         buy
         (let ((sell 
@@ -68,41 +64,38 @@
               0)))))
 
 (define (plant-crops pop acres grain)
-    (let ((limit (min acres (* grain 2) (* pop 10))))
-      (let loop ((msg (format "How many acres do you wish to plant (0-~a)?" limit)))
-        (displayln msg)
-        (let ((planted (read)))
-          (cond ((< planted 0) (resign))
-                ((> planted acres) (loop (format "Hammurabi: Think again. You own only ~a acres." acres)))
-                ((> (/ planted 2) grain) (loop (format "Hammurabi: Think again. You have only ~a bushels." grain)))
-                ((> planted (* 10 pop)) (loop (format "Hammurabi: But you have only ~a people to tend the fields!" pop)))
-                (else planted)
-                      )))))
+  (let ((limit (min acres (* grain 2) (* pop 10))))
+    (let loop ((msg (format "How many acres do you wish to plant (0-~a)?" limit)))
+      (displayln msg)
+      (let ((planted (read)))
+        (cond ((< planted 0) (resign))
+              ((> planted acres) (loop (format "Hammurabi: Think again. You own only ~a acres." acres)))
+              ((> (/ planted 2) grain) (loop (format "Hammurabi: Think again. You have only ~a bushels." grain)))
+              ((> planted (* 10 pop)) (loop (format "Hammurabi: But you have only ~a people to tend the fields!" pop)))
+              (else planted)
+              )))))
 
-;
-;    print()
-;    print('In your {}-year term of office, {} percent of the\n'
-;          'population starved per year on the average, i.e. a total of\n'
-;          '{} people died!!\n'.format(year, int(round(mortality_rate)), total_deaths))
-;
-;    if impeached or mortality_rate > 33 or acres_per_person < 7:
-;        print('Due to your extreme mismanagement you have not only\n'
-;              'been impeached and thrown out of office but you have\n'
-;              'also been declared national fink!!!!')
-;    elif mortality_rate > 10 or acres_per_person < 9:
-;        print('Your heavy-handed performance smacks of Nero and Ivan IV.\n'
-;              'The people (remaining) find you an unpleasant ruler, and,\n'
-;              'frankly, hate your guts!!')
-;    elif mortality_rate > 3 or acres_per_person < 10:
-;        haters = int(pop * random() * 0.8)
-;        print('Your performance could have been somewhat better, but\n'
-;              'really wasn\'t too bad at all. {} people\n'
-;              'dearly like to see you assassinated but we all have our\n'
-;              'trivial problems.'.format(haters))
-;    else:
-;        print('A fantastic performance!!! Charlemagne, Disraeli and\n'
-;              'Jefferson combined could not have done better!')
-;
-;    print('\nSo long for now.')
+(define (mortality-rate year starved pop) (exact-round (/ (+ (* (- year 1) mortality-rate) (/ (* starved 100) pop)) year)))
 
-(hammurabi)
+(define (end-game year pop acres total-deaths)
+  (let ((avg-mortality-rate (mortality-rate year total-deaths pop)))
+  (printf
+   "In your ~s-year term of office, ~s percent of the
+population starved per year on the average, i.e. a total of
+~s people died."  year mortality-rate total-deaths)
+  
+  (let ((acres-per-person (/ acres pop)))
+    (cond ((> avg-mortality-rate 33) (< acres-per-person 7))
+          (printf "Owing to extreme mismanagement you have been impeached")
+          ((or (> avg-mortality-rate 10) (< acres-per-person 9))
+           (printf "Your heavy-handed performance smacks of Nero and Ivan IV.
+The people (remaining) find you an unpleasant ruler"))
+          ((or (> avg-mortality-rate 3) (< acres-per-person 10))
+           (let ((haters (* pop (random) 0.8)))
+             (printf "Performance could have been somewhat better, ~s people
+would like to see you assassinated" haters)))
+          (else
+           (printf "Excellent performance"))))))
+
+(define (hammurabi)
+  (new-year 1 2 3 4 5 6 7 8 9 10))
