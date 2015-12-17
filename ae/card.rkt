bf5739476661dd23431334e6c5742fa934189ea1
Tom Walsh <walshtp@gmail.com>
2015-12-17 15:43:54 +0000

    add step-up and step-down

--- ae/card.rkt
+++ ae/card.rkt
@@ -8,20 +8,26 @@
         (reverse lines)
         (loop (cons line lines))))))
 
-(define store (make-vector 10 0))
+(define store (make-vector 20 0))
 (define (put col val)
   (vector-set! store (string->number col) (string->number val)))
 (define (get col)
   (vector-ref store (string->number col)))
 
-(define egress 0)
-(define egressp 0)
-
 (define (divn ingress)
   (match-let (((list x y) (reverse ingress)))
     (let-values (((q r) (quotient/remainder x y)))
       (list q r))))
-    
+
+(define (mult ingress)
+  (list 0 (apply * ingress)))
+
+(define (step-down egress shift)
+  (map (lambda (n) (/ n (expt 10 shift))) egress))
+
+(define (step-up ingress shift)
+  (map (lambda (n) (* n (expt 10 shift))) ingress))
+
 (define (read-card lines)
   (let loop ((cards lines) (ingress '()) (op (lambda (a b) '())) (egress '()))
     (if (= (length ingress) 2)
@@ -41,20 +47,28 @@
                       ((eq? instr #\-)
                        (loop (cdr cards) ingress - egress))
                       ((eq? instr #\*)
-                       (loop (cdr cards) ingress * egress))
+                       (loop (cdr cards) ingress mult egress))
                       ((eq? instr #\/)
                        (loop (cdr cards) ingress divn egress))
                       ((eq? instr #\L)
                        (let ((col (substring line 1)))
-                         (printf "L ~s~n" col)
+                         (loop (cdr cards) (cons (get col) ingress) op egress)))
+                      ((eq? instr #\Z)
+                       (let ((col (substring line 1)))
+                         (put col 0)
                          (loop (cdr cards) (cons (get col) ingress) op egress)))
                       ((eq? instr #\S)
                        (let ((col (string->number (substring line 1 4))) (prime (string-ref line 4)))
-                         (printf "S ~s ~s~n" col prime)
                          (if (eq? prime #\')
                              (vector-set! store col (car egress))
                              (vector-set! store col (cadr egress)))
                          (loop (cdr cards) ingress op egress)))
+                      ((eq? instr #\>)
+                       (let ((shift (string->number (substring line 1))))
+                         (loop (cdr cards) ingress op (step-down egress shift))))
+                      ((eq? instr #\<)
+                       (let ((shift (string->number (substring line 1))))
+                         (loop (cdr cards) (step-up ingress shift) op egress)))
                       )))
             (printf "I ~s O ~s E ~s~n" ingress op egress)
         ))))
@@ -67,6 +81,20 @@
                 "L002"
                 "S003'"
                 "S004 "
+
+                "N010 4000000000"
+                "N011 2500000"
+                "N012 28000000"
+                "*"
+                "L010"
+                "L011"
+                ">6"
+                "S013 "
+                "/"
+                "L013"
+                "<6"
+                "L012"
+                "S014'"
                 ))
 
 cards
