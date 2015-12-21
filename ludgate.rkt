9134c97c6321d327145a7089c79106b11a85a12c
Tom Walsh <walshtp@gmail.com>
2015-12-21 15:32:53 +0000

    Add ludgate.rkt

---
+++ ludgate.rkt
@@ -0,0 +1,50 @@
+#lang racket
+                        ; 0 1 2 3 4  5 6  7 8  9
+(define simple-index  #( 50 0 1 7 2 23 8 33 3 14 ))
+
+(define partial-product (hash 0 1 1 2 2 4 3 8 4 16 5 32 6 64 7 3 8 6 9 12
+                              10 24 11 48 12 -1 13 -1 14 9 15 18 16 36 17 72
+                              18 -1 19 -1 20 -1 21 27 22 54 23 5 24 10 25 20
+                              26 40 27 -1 28 81 29 -1 30 15 31 30 32 -1 33 7
+                              34 14 35 28 36 56 37 45 38 -1 39 -1 40 21 41 42
+                              42 -1 43 -1 44 -1 45 -1 46 25 47 63 48 -1 49 -1
+                              50 0
+                              51 2
+                              52 4
+                              53 8
+                              54 -1 55 -1 56 35
+                              57 3
+                              58 6
+                              59 -1 60 -1 61 -1 62 -1 63 -1
+                              64 9
+                              65 -1 66 49
+                              73 5
+                              83 7
+                              100 1 ))
+
+(define (num2digits n0)
+  (let loop ((digits '()) (n n0))
+    (if (>= n 1)
+        (loop (cons (remainder n 10) digits) (floor (/ n 10)))
+        digits
+        )))
+  
+(define n1 523)
+
+(define d1 (num2digits n1))
+
+(printf "~s ~s~n" n1 d1)
+
+(define (mul n1 n2)
+   (printf "~s ~s -> " n1 n2)
+  (let ((i1 (vector-ref simple-index n1))
+        (i2 (vector-ref simple-index n2)))
+    (let ((ci (+ i1 i2)))
+     
+      (let ((pp (hash-ref partial-product ci)))
+         (printf "~s ~s ~s ~s -> " n1 n2 ci pp)
+        pp))))
+
+(for* ((i (range 1 10))
+       (j (range 1 10)))
+  (printf "~s ~s ~s ~s~n" i j (* i j) (mul i j)))
