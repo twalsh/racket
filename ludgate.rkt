57d184b47d710406127dcfe3348c02d195b4d80b
Tom Walsh <walshtp@gmail.com>
2015-12-22 11:48:31 +0000

    Add comments

--- ludgate.rkt
+++ ludgate.rkt
@@ -1,5 +1,10 @@
 #lang racket
-; 0 1 2 3 4  5 6  7 8  9
+
+; Ludgate's algorithm for multiplying integers using partial products
+; and a lookup table.
+
+(require rackunit)
+;                         0 1 2 3 4  5 6  7 8  9
 (define simple-index  #( 50 0 1 7 2 23 8 33 3 14 ))
 
 (define product-table (hash 0 1 1 2 2 4 3 8 4 16 5 32 6 64 7 3 8 6 9 12
@@ -9,19 +14,20 @@
                             34 14 35 28 36 56 37 45 38 -1 39 -1 40 21 41 42
                             42 -1 43 -1 44 -1 45 -1 46 25 47 63 48 -1 49 -1
                             50 0
-                            51 2
-                            52 4
-                            53 8
+                            51 0
+                            52 0
+                            53 0
                             54 -1 55 -1 56 35
-                            57 3
-                            58 6
+                            57 0
+                            58 0
                             59 -1 60 -1 61 -1 62 -1 63 -1
-                            64 9
+                            64 0
                             65 -1 66 49
-                            73 5
-                            83 7
-                            100 1 ))
+                            73 0
+                            83 0
+                            100 0 ))
 
+; Convert integer to list of digits
 (define (num2digits n0)
   (let loop ((digits '()) (n n0))
     (if (>= n 1)
@@ -29,42 +35,37 @@
         digits
         )))
 
-(define n1 523)
-
-(define d1 (num2digits n1))
-
-(printf "~s ~s~n" n1 d1)
-
+; Takes a list of integers and shifts them (multiplies
+; them by powers of ten), the power increasing from the
+; tail to the head of list. The return value is the sum
+; of the shifted list.
 (define (shift products)
   (apply + (for/list ((i (in-range (length products)))
                       (p (reverse products)))
              (* p (expt 10 i)))))
 
-(define (partial-product d1 digits)
-  
-  (let 
-      ((pprod
-        
-        (for/list ((d2 digits))
-          (let ((i1 (vector-ref simple-index d1))
-                (i2 (vector-ref simple-index d2)))
-            (let ((ci (+ i1 i2)))
-              (hash-ref product-table ci))
-            ))))
-    
-    (shift pprod)
-    ))
 
+; Compute partial product of multiplying the multiplicand
+; (represented as a list of its digits) by a digit of th
+; multiplier.
+(define (partial-product d1 digits)
+  (shift
+   (for/list ((d2 digits))
+     (let ((i1 (vector-ref simple-index d1))
+           (i2 (vector-ref simple-index d2)))
+       (let ((ci (+ i1 i2)))
+         (hash-ref product-table ci))
+       ))))
+
+; Multiply two integers by the method of partial products
 (define (mul n1 n2)
-  (printf "MUL ~s ~s:~n" n1 n2)
   (let ((digits1 (num2digits n1))
         (digits2 (num2digits n2)))
     (shift (map (lambda (d) (partial-product d digits2)) digits1))))
 
-(for* ((i (range 1 10))
-       (j (range 1 10)))
-  (printf "~s ~s ~s ~s~n" i j (* i j) (mul i j)))
-
-(define i 12)
-(define j 21)
-(printf "MD ~s MR ~s E ~s A ~s~n" i j (* i j) (mul i j))
+; Check that the 12-times tables is computed correctly
+(for* ((i (range 1 12))
+       (j (range 1 12)))
+  (printf "~s x ~s = ~s~n" i j (mul i j))
+  (check-equal? (mul i j) (* i j) (format "~s x ~s = ~s" i j (* i j)))
+  )
