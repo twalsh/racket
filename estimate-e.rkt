51f3f30be3dae612e10a7f8d96b89333dee32ad2
Tom Walsh <walshtp@gmail.com>
2016-06-19 00:03:23 +0100

    Simplify code

--- estimate-e.rkt
+++ estimate-e.rkt
@@ -3,17 +3,13 @@
 (define squares (* 8 8)) 
 
 (define (estimate-e)
-  (define board (make-vector squares 0))
-  
-  (for ((i (in-range squares)))
-    (let ((square (random squares)))
-      (vector-set! board square (add1 (vector-ref board square)))))
-  
-  board
-  
-  (exact->inexact (/ squares (vector-count zero? board))))
+  (let ((board (make-vector squares 0)))
+    (for ((i (in-range squares)))
+      (let ((square (random squares)))
+        (vector-set! board square 1)))
+    (exact->inexact (/ squares (vector-count zero? board)))))
 
-(define trials 1000)
+(define trials 1000) 
 
 (/ (for/sum ((i (in-range trials)))
      (estimate-e))
