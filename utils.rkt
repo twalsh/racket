3b6e106adb568a8adda72ab6c94be03678f344d0
Tom Walsh <walshtp@gmail.com>
2016-06-23 11:21:24 +0100

    Add methods

--- utils.rkt
+++ utils.rkt
@@ -19,14 +19,20 @@
     (define _data data)
     (super-new)
     (define/public (get-header) _header)
-    (define/public (get-data) _data)))
+    (define/public (get-data) _data)
+    (define/public (get-column-index column-name)
+      (hash-ref _header column-name))
+    (define/public (get-column column-name)
+      (define column-index (hash-ref _header column-name))
+      (map (lambda (row) (vector-ref row column-index)) _data))
+    ))
 
 (define (text-csv-read file)  
   (let ((lines (read-input file)))
     ; Split lines into fields
-    (let ((line-fields (map (lambda (line) (string-split line ",")) lines)))
+    (let ((line-fields (map (lambda (line) (list->vector (string-split line ","))) lines)))
       ; Make vector of field names from first line
-      (let ((field-names (list->vector (first line-fields))))
+      (let ((field-names (first line-fields)))
         ; Create hash mapping field names to numbers
         (let ((dict (for/hash ((i (in-range (vector-length field-names))))
                       (values (vector-ref field-names i) i))))
