2ea8ee35d9c72ffe7acdb1a03a4d8945fe165eb8
Tom Walsh <walshtp@gmail.com>
2016-11-17 17:15:43 +0000

    Update

--- gray.rkt
+++ gray.rkt
@@ -3,17 +3,23 @@
 (define (gray-code n)
   (define (make-layer n)
     (if (= n 1)
-        (list '(0 1))
+        (list '(#\0 #\1))
         (let ((ld (make-layer (sub1 n))))
           (define mirror
             (map (lambda (l) (flatten (list l (reverse l))))
                  ld))
-          (define next-level (flatten (list (make-list n 0) (make-list n 1))))
-          (printf "~a~n" mirror)
-          (printf "~a~n" next-level)
+          (define nlw (expt 2 (sub1 n)))
+          (define next-level (flatten (list (make-list nlw #\0) (make-list nlw #\1))))
           (cons next-level mirror))))
   (define layers (make-layer n))
-  layers)
+  (reverse 
+   (let loop ((layers layers) (codes '()))
+     (if (empty? (first layers))
+         codes
+         (let ((next-code
+                (list->string (map car layers))))
+           (loop (map rest layers) (cons next-code codes))
+           )))))
         
 (gray-code 1)
 (gray-code 2)
