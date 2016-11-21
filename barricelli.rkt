4e1a54b7ccaaacafec1ef85a9d70c6937d1202a6
Tom Walsh <walshtp@gmail.com>
2016-11-21 16:59:01 +0000

    update

--- barricelli.rkt
+++ barricelli.rkt
@@ -1,32 +1,22 @@
 #lang racket
 
-;(define size 16)
+(define first-generation (vector 4 0 0 0 0 3 0 0 0 -2))
+(define size (vector-length first-generation))
 
-(define this-generation (vector 4 0 0 0 0 0 0 0 0 -2))
-
-(define size (vector-length this-generation))
-
-;(define this-generation
-; (for/vector ((i (in-range size)))
-;  (- size (random (* size 2)))))
-
-(define next-generation (make-vector size 0))
-
-this-generation
-
-(for ((_ (in-range 4)))
-  (for ((i (in-range size)))
-    (define n (vector-ref this-generation i))
-    (let reproduce ((j n))
-      (unless (= j 0)
-        (let ((k (abs (remainder (+ i j) size))))
-          (printf "i: ~a j: ~a k:~a ~n" i j k)
-          (unless (> (vector-ref next-generation k) 0)
-            (printf "k ~a n: ~a ~n" k n)
-            (vector-set! next-generation k n)
-           
-            (reproduce (vector-ref this-generation k)))))))
-
-  (vector-copy! this-generation 0 next-generation)
-  (vector-fill! next-generation 0)
-  (displayln this-generation))
+(let loop ((g 0) (this-generation first-generation))
+  (when (< g 5)
+    (let ((next-generation (make-vector size 0)))
+      (for ((i (in-range size)))
+        (define n (vector-ref this-generation i))
+        (let reproduce ((j n))
+          (unless (= j 0)
+            (let ((k (abs (remainder (+ i j) size))))
+              
+              (unless (> (vector-ref next-generation k) 0)
+                (vector-set! next-generation k n))
+              (let ((next-gene (vector-ref this-generation k)))
+                (printf "i ~a j ~a k ~a n ~a ng ~a~n" i j k n next-gene)
+                (unless (= next-gene j)
+                  (reproduce (vector-ref this-generation k))))))))
+      (displayln this-generation)
+      (loop (add1 g) next-generation))))
